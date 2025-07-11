a706ad0ca7fbeffe6a6ad2751a592017d1cff285
Aashi Ashfak <150011814+aashiashfak@users.noreply.github.com>
2025-07-11 05:52:57 +0530

    created user model and completed authentication views

---
+++ accounts/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from .models import CustomUser
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "email",
+        "username",
+        "role",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "created_at",
+        "updated_at",
+    )
+
+    search_fields = ("email", "username", "role")
+
+    list_filter = ("role", "is_active", "is_staff", "is_superuser")
+
+    ordering = ("-created_at",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
